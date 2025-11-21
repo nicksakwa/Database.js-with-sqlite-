428b88d6c2ce69761a2a3bb51e622ea47df9f2c0
nicksakwa <nsakwa@gmail.com>
2025-11-21 16:15:06 +0300

    create db & insert dat

---
+++ index.js
@@ -0,0 +1,24 @@
+import { Database } from '.database-js';
+const connectionString ='sqlite://my-databse.sqlite';
+const db= new Database(connectionString);
+async function runDatabaseApp(){
+    console.log('---1. Initializing Database and Creating table---');
+    try{
+        const createResult= await.db.query('
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT null,
+                email TEXT UNIQUE NOT null
+            );
+        ');
+        console.log('Table users ensured to exist.');
+        console.log('\n--- 2. Inserting Data into Table---);
+        const userName='John Doe';
+        const userEmail='johndoe@example.com';
+        const insertResult= await db.insert('
+            INSERT INTO users(name, email) VALUES(?, ?);
+            ', [userName, userEmail]);
+        console.log('Successfully inserted user: ${userName} with ID: ${insertResult.LastInsertID}');
+    )
+    }
+}
